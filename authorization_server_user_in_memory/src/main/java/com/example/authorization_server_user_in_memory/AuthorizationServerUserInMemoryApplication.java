4ab593bbdd26dc82327925aeb9d56521aaeb782e
AnatolSalau <salovanatoly5@mail.ru>
2023-04-18 15:49:46 +0300

    Initial commit

---
+++ authorization_server_user_in_memory/src/main/java/com/example/authorization_server_user_in_memory/AuthorizationServerUserInMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.authorization_server_user_in_memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerUserInMemoryApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(AuthorizationServerUserInMemoryApplication.class, args);
+      }
+
+}
---
+++ authorization_server_user_in_memory/src/test/java/com/example/authorization_server_user_in_memory/AuthorizationServerUserInMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authorization_server_user_in_memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerUserInMemoryApplicationTests {
+
+      @Test
+      void contextLoads() {
+      }
+
+}
