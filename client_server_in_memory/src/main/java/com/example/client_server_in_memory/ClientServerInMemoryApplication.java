a984fe47fb90e7ef76bcbda7d4e18a1004d416fc
AnatolSalau <salovanatoly5@mail.ru>
2023-04-19 16:03:11 +0300

    Client server in memory

---
+++ client_server_in_memory/src/main/java/com/example/client_server_in_memory/ClientServerInMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.client_server_in_memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServerInMemoryApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(ClientServerInMemoryApplication.class, args);
+      }
+
+}
--- resource_server_in_memory/src/main/java/com/example/resource_server_in_memory/configurations/ResourceServerConfig.java
+++ resource_server_in_memory/src/main/java/com/example/resource_server_in_memory/configurations/ResourceServerConfig.java
@@ -2,14 +2,8 @@ package com.example.resource_server_in_memory.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Configuration
 public class ResourceServerConfig {
